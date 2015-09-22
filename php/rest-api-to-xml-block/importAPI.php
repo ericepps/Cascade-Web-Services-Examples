f210bbf040e1d5023294e8a1c34ae9cb5c33a116
Eric L. Epps <eric@ericepps.me>
2015-09-22 01:22:48 -0400

    Update importAPI.php
    change db query to array for demonstration

--- php/rest-api-to-xml-block/importAPI.php
+++ php/rest-api-to-xml-block/importAPI.php
@@ -11,18 +11,12 @@ function to_xml(SimpleXMLElement $object, array $data) {
     }   
 }   
 
-function createCascadeCareer($cascade,$auth,$blockPath,$blockName,$theData,$dataType) {
-	$path = array('path' => $blockPath.'/'.$blockName, 'siteName' => 'www.svcc.edu');
+function createCascadeCareer($cascade,$auth,$blockPath,$blockName,$siteName,$theData,$dataType) {
+	$path = array('path' => $blockPath.'/'.$blockName, 'siteName' => $siteName);
 	$id = array('path' => $path, 'type' => 'block');
 	$readParams = array( 'authentication' => $auth, 'identifier' => $id );	
 	if($dataType == 'xml') {
 		$xmlData = $theData;
-		$xmlData = str_replace(' xmlns:xsd="http://www.w3.org/2001/XMLSchema"','',
-							str_replace(' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"','',
-							str_replace(' xmlns="http://www.careerinfonet.org/webservices/occupationData"','',
-							str_replace(' xmlns="http://www.careerinfonet.org/WebServices/OccWages_WebService/OccWagesService.asmx"','',
-							str_replace('<?xml version="1.0" encoding="utf-8"?>','',$xmlData)
-						  ))));
 	} elseif ($dataType == 'json') {
 		$jsonData = json_decode($theData,true);
 		$xml = new SimpleXMLElement('<root/>');
@@ -60,25 +54,23 @@ function createCascadeCareer($cascade,$auth,$blockPath,$blockName,$theData,$data
 
 $restURL = 'https://api.data.gov/ed/collegescorecard/v1/schools.json?id=148672,147703&_fields=school.name,school.city,school.state,id&api_key={{secret}}';
 $scJSON = file_get_contents($restURL);
-createCascadeCareer($cascade,$auth, '/-blocks/CareerOneStop', 'scorecard', $scJSON,'json');
+createCascadeCareer($cascade,$auth, '/-blocks/CareerOneStop', 'scorecard', $siteName, $scJSON,'json');
 		$jsonData = json_decode($scJSON);
 
 
 $zipCode = '99999';
-$apiKey = '[[secret}}';
+$apiKey = '{{secret}}';
+$socCodes = array('17-2011','41-9041');
 
-
-$query = 'SELECT DISTINCT socCode FROM cipPrograms LEFT JOIN cipClusters ON cipPrograms.cipCode = cipClusters.cipCode WHERE NOT ISNULL(socCode)';
-$result = mysqli_query($mysqli,$query);
-while ($row = mysqli_fetch_assoc($result)) {
-	$socCode = str_replace('-','',$row['socCode']);
+foreach($socCodes as $value) {
+	$socCode = str_replace('-','',$value);
 	$soapURL = 'http://www.careerinfonet.org/webservices/occwages_webservice/occwagesservice.asmx/GetWagesByZip?userID='.$apiKey.'&soccode='.$socCode.'&zip='.$zipCode;
 	$wagesXML = file_get_contents($soapURL);
-	createCascadeCareer($cascade,$auth, '/-blocks/CareerOneStop/Wage', $row['socCode'], $wagesXML,'xml');
+	createCascadeCareer($cascade,$auth, '/-blocks/CareerOneStop/Wage', $value, $siteName, $wagesXML,'xml');
 	
-	$soapURL = 'http://www.careerinfonet.org/webservices/occupationdata/occupationdata.asmx/getOccT2Data?userID='.$apiKey.'&onetcode='.$row['socCode'].'.00';
+	$soapURL = 'http://www.careerinfonet.org/webservices/occupationdata/occupationdata.asmx/getOccT2Data?userID='.$apiKey.'&onetcode='.$value.'.00';
 	$profileXML = file_get_contents($soapURL);
-	createCascadeCareer($cascade,$auth, '/-blocks/CareerOneStop/Profile', $row['socCode'], $profileXML,'xml');
+	createCascadeCareer($cascade,$auth, '/-blocks/CareerOneStop/Profile', $row['socCode'], $siteName, $profileXML,'xml');
 }
 
 ?>
